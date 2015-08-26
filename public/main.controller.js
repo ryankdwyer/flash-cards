71c891e691dadca6ca382623e72e29c20232ca8a
Ryan Dwyer <ryankdwyer@gmail.com>
2015-08-26 17:15:24 -0400

    First commit

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,30 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, FlashCardsFactory) {
 
-	$scope.answerQuestion = function (answer, flashCard) {
-		if (!flashCard.answered) {
-			flashCard.answered = true;
-			flashCard.answeredCorrectly = answer.correct;
-		}
-	}
+    FlashCardsFactory.getFlashCards()
+        .then(function (data) {
+            console.log(data);
+            $scope.flashCards = data;
+        });
+
+    $scope.categories = [
+        'MongoDB',
+        'Express',
+        'Angular',
+        'Node'
+    ];
+
+    $scope.getCategoryCards = function (category) {
+        FlashCardsFactory.getFlashCards(category)
+            .then(function (data) {
+                console.log(data);
+                $scope.flashCards = data;
+            });
+    };
+
+    $scope.answerQuestion = function (answer, flashCard) {
+        if (!flashCard.answered) {
+            flashCard.answered = true;
+            flashCard.answeredCorrectly = answer.correct;
+        }
+    };
 });
